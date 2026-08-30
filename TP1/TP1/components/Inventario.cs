49890d040eca3a8e3f1712f1e7a2c9ac38eabdb0
Gonzalo Thomas Lazarte <gonzalo.lazt.dev@gmail.com>
2026-08-30 02:26:21 -0300

    added functionality to Inventario.cs

--- TP1/TP1/components/Inventario.cs
+++ TP1/TP1/components/Inventario.cs
@@ -1,18 +1,32 @@
 ﻿namespace TP1.components;
 
 public class Inventario {
-    public List<Pocion> pociones = new List<Pocion>();
-    public int capacidadMaxima = 3;
+    public List<Pocion> pociones = new List<Pocion>(); //lista que guardara objetos del tipo pocion
+    private int capacidadMaxima = 3;
 
-
-    public void agruegarPocion() {
-
+    public void agregarPocion(Pocion pocionInv) {
+        if (pociones.Count() < capacidadMaxima && pociones.Count() >= 0) {
+            pociones.Add(pocionInv);
+        }
+        else {
+            Console.WriteLine("Inventario lleno");
+        }
     }
-    public void usarPocion() {
+    public void usarPocion(int pocionSelec, Jugador player) {
+        int pocionesDisp = pociones.Count();
+        if (pocionSelec > pocionesDisp - 1 && pocionSelec < 0) {
+            Console.WriteLine("Vacio...");
+        }
+        else {
+            pociones[pocionSelec - 1].usar(player);
+            pociones.RemoveAt(pocionSelec - 1);
+        }
 
     }
     public void listarPociones() {
-
+        foreach (Pocion pocion in pociones) {
+            pocion.mostrarDetalle();
+        }
     }
 }
 
