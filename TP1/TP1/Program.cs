30a43388b0dae43603bfcdab25c7390ee9d11cc7
Gonzalo Thomas Lazarte <gonzalo.lazt.dev@gmail.com>
2026-09-01 08:43:29 -0300

    added functionality to Flujo.cs

--- TP1/TP1/Program.cs
+++ TP1/TP1/Program.cs
@@ -1,7 +1,4 @@
 ﻿using TP1.components;
 
-Pocion item1 = new Pocion("Poción de Salud", "salud", 50, 10);
-Reliquia Objeto = new Reliquia();
-
-item1.usar();
-Objeto.usar();
+Random random = new Random();
+Flujo FlujoDeJuego = new Flujo(random);
--- TP1/TP1/components/Flujo.cs
+++ TP1/TP1/components/Flujo.cs
@@ -1,2 +1,35 @@
-﻿
-namespace TP1.components;
+﻿namespace TP1.components;
+
+class Flujo {
+    // que recibe esta clase? 
+    public int PisosTotales; 
+
+    public Flujo (Random random) {
+        PisosTotales = random.Next(10, 31); 
+    }
+
+    public void TipoDePiso(Random random) {
+        int Tipo = random.Next(1, 6);
+        switch (Tipo) {
+            case 1 :
+                // logica que defina el tipo de sala
+                break;
+            case 2 :
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+        }
+
+
+    }
+
+
+
+
+}
+
+
